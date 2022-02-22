2c4ac2979639976fa411af5bb08aa990942fe8ed
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-02-22 08:32:45 +0530

    chore: changed file structure

---
+++ documentation/index.js
@@ -0,0 +1,15 @@
+const asideBar = document.getElementById("aside");
+const asideContainer = document.getElementById("aside-container");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", (event) => {
+  if (asideBar.style.display == "none") {
+    asideBar.style.display = "block";
+    event.preventDefault();
+    document.body.style.overflow = "hidden";
+  } else {
+    asideBar.style.display = "none";
+    event.preventDefault();
+    document.body.style.overflow = "visible";
+  }
+});
